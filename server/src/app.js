8502d93891e6d4272fdebc73e49e11f0788b374b
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-01-02 16:52:28 +0100

    fix CORS error for requests to planet routes using CORS middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,10 +1,16 @@
 // Setting up express server
 const express = require("express");
 const planetsRouter = require("./routes/planets/planets.router");
+const cors = require("cors");
 
 const app = express();
 
 // Express APP MIDDLEWARES
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 app.use(planetsRouter);
 
