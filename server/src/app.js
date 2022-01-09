99283d232bace599a9dc720f191574c7f410b619
KokoLuciferus <abdulhakeemadefioye@gmail.com>
2022-01-09 07:15:49 +0100

    automating script for installing dependencies,serving frontend with express and starting server

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,9 @@
 // Setting up express server
+const path = require("path");
 const express = require("express");
 const planetsRouter = require("./routes/planets/planets.router");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 
@@ -11,7 +13,14 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
 app.use(express.json());
+// To serve image, css, js files in the public folder(Folder housing React app)
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(planetsRouter);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
